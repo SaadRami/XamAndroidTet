a13a1be215393f9f996db7c01962c6061714f837
saad <sr.saadrami@gmail.com>
2021-03-16 15:45:29 +0100

    fComm

---
+++ XamMobileApp/XamMobileApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamMobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DefinirUtilisateur(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new Views.EnregistrerUtilisateur());
+        }
+
+        protected override void OnAppearing()
+        {
+            SetWelcomeMessage();
+        }
+
+        private void SetWelcomeMessage()
+        {
+            String StrWelcomeMessage = "Bienvenue dans l'application ";
+
+            if (Application.Current.Properties.ContainsKey("username"))
+            {
+                StrWelcomeMessage += Application.Current.Properties["username"] as string;
+            }
+            else
+            {
+                StrWelcomeMessage += "Utilisateur non enregistré";
+            }
+
+            welcomeMessage.Text = StrWelcomeMessage;
+        }
+    }
+}
---
+++ XamMobileApp/XamMobileApp/Views/EnregistrerUtilisateur.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamMobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnregistrerUtilisateur : ContentPage
+    {
+        public EnregistrerUtilisateur()
+        {
+            InitializeComponent();
+        }
+
+        private void EnregistrerPrenom(object sender, EventArgs e)
+        {
+            Application.Current.Properties["username"] = this.prenom.Text;
+            this.Navigation.PopAsync();
+        }
+    }
+}
